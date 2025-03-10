422761e526f7f8d1d7111d4739eb0043b88d2959
Safreen97 <92501786+Safreen97@users.noreply.github.com>
2025-03-10 09:43:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$api_url = "https://ltaattendance.wuaze.com/fetch_employee_API.php";
+$api_url = "https://ltaattendance.wuaze.com/fetch_employee_API.php"; 
 
 // Use cURL to fetch the API data
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $api_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
-curl_close($ch);
 
-echo $response;
+if (curl_errno($ch)) {
+    echo json_encode(["error" => curl_error($ch)]);
+} else {
+    echo $response;
+}
+
+curl_close($ch);
 ?>
