ca93356d04c4297793f9b8eeb7f778ea9cc9ff8a
Safreen97 <92501786+Safreen97@users.noreply.github.com>
2025-03-10 09:39:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-  echo "Hello from Render PHP!";
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+$api_url = "https://ltaattendance.wuaze.com/fetch_employee_API.php";
+
+// Use cURL to fetch the API data
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $api_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
 ?>
