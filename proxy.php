e21bb2480e19f3fe761fc33d9ee67d6340739a78
Safreen97 <92501786+Safreen97@users.noreply.github.com>
2025-03-10 10:12:13 +0800

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -2,22 +2,14 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-// Target URL (your InfinityFree API)
-$api_url = "http://ltaattendance.wuaze.com/fetch_employee_API.php"; 
+$target_url = "http://ltaattendance.byethost7.com/fetch_employee_API.php?i=2";
 
-// Initialize cURL
-$ch = curl_init($api_url);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $target_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-// Handle cookies to bypass InfinityFree's security check
-curl_setopt($ch, CURLOPT_COOKIEJAR, __DIR__ . "/cookies.txt");
-curl_setopt($ch, CURLOPT_COOKIEFILE, __DIR__ . "/cookies.txt");
-
-// Execute the request
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $response = curl_exec($ch);
 curl_close($ch);
 
-// Return the response
 echo $response;
 ?>
